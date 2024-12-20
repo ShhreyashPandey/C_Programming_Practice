d64a3e561fad40b0fc19a14eac3fb1b399da2d42
ShhreyashPandey <129041058+ShhreyashPandey@users.noreply.github.com>
2024-12-20 14:26:53 +0530

    Create QuickSort.c

---
+++ Array/QuickSort.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+void merge(int arr[], int l, int m, int r) {
+    int i=0, j=0, k=l,n1 = m - l + 1,n2 = r - m;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } 
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+int main() {
+    srand(time(NULL));
+
+    int n = 10000;
+    int arrQuickSort[n];
+    int arrMergeSort[n];
+    
+    for (int i = 0; i < n; i++) {
+        arrQuickSort[i] = arrMergeSort[i] = rand() % 100000;
+    }
+    clock_t start, end;
+    // Quick Sort
+    start = clock();
+    quickSort(arrQuickSort, 0, n - 1);
+    end = clock();
+    printf("Quick Sort Time: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+    // Merge Sort
+    start = clock();
+    mergeSort(arrMergeSort, 0, n - 1);
+    end = clock();
+    printf("Merge Sort Time: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+    return 0;
+}
